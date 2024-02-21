ff32446ef09c2b0eccfd8f6443d1cb420ced7a24
Dmitrii Smirnov <dmitrijsmirnov@MacBook-Pro-Ultra.local>
2024-02-21 16:47:22 +0400

    first commit. what

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const content = document.querySelector('.places');
+const cardsList = places.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(name, link, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    //const deleteButton = cardElement.querySelector('.card__delete-button').addEventListener('click', deleteButton);
+
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__image').src = link;
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((elem) => {
+})
