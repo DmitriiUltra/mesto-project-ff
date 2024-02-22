9812ef21255e90d55edc5eb7fff7823ee2213628
Dmitrii Smirnov <dmitrijsmirnov@MacBook-Pro-Ultra.local>
2024-02-22 22:25:08 +0400

    second commit

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,30 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const content = document.querySelector('.places');
-const cardsList = places.querySelector('.places__list');
+const cardsList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function createCard(name, link, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    //const deleteButton = cardElement.querySelector('.card__delete-button').addEventListener('click', deleteButton);
-
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    
     cardElement.querySelector('.card__title').textContent = name;
     cardElement.querySelector('.card__image').src = link;
+
+    deleteButton.addEventListener('click', deleteCard);
 
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    const deleteItem = event.target.closest('.card');
+
+    deleteItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach((elem) => {
+initialCards.forEach((item) => {
+    cardElement = createCard(item.name, item.link, deleteCard);
+    cardsList.append(cardElement);
 })
