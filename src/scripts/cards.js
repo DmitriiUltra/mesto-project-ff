f44247009b60800a2405cc73782a917b4c4efc0e
Dmitrii Smirnov <dmitrijsmirnov@MacBook-Pro-Ultra.local>
2024-03-12 20:38:38 +0400

    7th.3 - broken boot

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,37 +1,3 @@
-// import arhyzImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
-// import chelOblImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg';
-// import ivanovoImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg';
-// import kamchatkaImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg';
-// import kholmAreaImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg';
-// import baykalImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg';
-
-// const initialCards = [
-//     { name: 'Архыз', link: arhyzImage },
-//     { name: 'Челябинская область', link: chelOblImage },
-//     { name: 'Иваново', link: ivanovoImage },
-//     { name: "Камчатка", link: kamchatkaImage },
-//     { name: "Холмогорский район", link: kholmAreaImage },
-//     { name: "Байкал", link: baykalImage }
-// ];
-
-
-const arhyzImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg', import.meta.url);
-const chelOblImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg', import.meta.url);
-const ivanovoImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg', import.meta.url);
-const kamchatkaImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg', import.meta.url);
-const kholmAreaImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg', import.meta.url);
-const baykalImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg', import.meta.url);
-
-const initialCards = [
-    { name: 'Архыз', link: arhyzImage },
-    { name: 'Челябинская область', link: chelOblImage },
-    { name: 'Иваново', link: ivanovoImage },
-    { name: "Камчатка", link: kamchatkaImage },
-    { name: "Холмогорский район", link: kholmAreaImage },
-    { name: "Байкал", link: baykalImage }
-];
-
-
 // const initialCards = [
 //   {
 //     name: "Архыз",
@@ -59,4 +25,60 @@ const initialCards = [
 //   }
 // ];
 
-export { initialCards };
+
+
+// import arhyzImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg';
+// import chelOblImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg';
+// import ivanovoImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg';
+// import kamchatkaImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg';
+// import kholmAreaImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg';
+// import baykalImage from 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg';
+
+// const initialCards = [
+//     { name: 'Архыз', link: arhyzImage },
+//     { name: 'Челябинская область', link: chelOblImage },
+//     { name: 'Иваново', link: ivanovoImage },
+//     { name: "Камчатка", link: kamchatkaImage },
+//     { name: "Холмогорский район", link: kholmAreaImage },
+//     { name: "Байкал", link: baykalImage }
+// ];
+
+
+
+const arhyzImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg', import.meta.url);
+const chelOblImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg', import.meta.url);
+const ivanovoImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg', import.meta.url);
+const kamchatkaImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg', import.meta.url);
+const kholmAreaImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg', import.meta.url);
+const baykalImage = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg', import.meta.url);
+
+const initialCards = [
+    { name: 'Архыз', link: arhyzImage },
+    { name: 'Челябинская область', link: chelOblImage },
+    { name: 'Иваново', link: ivanovoImage },
+    { name: "Камчатка", link: kamchatkaImage },
+    { name: "Холмогорский район", link: kholmAreaImage },
+    { name: "Байкал", link: baykalImage }
+];
+
+// @todo: Функция создания карточки
+function createCard(name, link, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
+}
+
+// @todo: Функция удаления карточки
+function deleteCard(event) {
+  const deleteItem = event.target.closest('.card');
+  deleteItem.remove();
+}
+
+export { createCard, deleteCard, initialCards };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,31 +1,31 @@
-import { initialCards } from './cards.js';
 import '../pages/index.css';
+import { createCard, deleteCard, initialCards } from './cards.js';
 
-// @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+// // @todo: Темплейт карточки
+// const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const content = document.querySelector('.places');
-const cardsList = content.querySelector('.places__list');
+const cardsList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
-function createCard(name, link, deleteCard) {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const deleteButton = cardElement.querySelector('.card__delete-button');
+// // @todo: Функция создания карточки
+// function createCard(name, link, deleteCard) {
+//     const cardTemplate = document.querySelector('#card-template').content;
+//     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+//     const deleteButton = cardElement.querySelector('.card__delete-button');
     
-    cardElement.querySelector('.card__title').textContent = name;
-    cardElement.querySelector('.card__image').src = link;
+//     cardElement.querySelector('.card__title').textContent = name;
+//     cardElement.querySelector('.card__image').src = link;
 
-    deleteButton.addEventListener('click', deleteCard);
+//     deleteButton.addEventListener('click', deleteCard);
 
-    return cardElement;
-}
+//     return cardElement;
+// }
 
-// @todo: Функция удаления карточки
-function deleteCard(event) {
-    const deleteItem = event.target.closest('.card');
-    deleteItem.remove();
-}
+// // @todo: Функция удаления карточки
+// function deleteCard(event) {
+//     const deleteItem = event.target.closest('.card');
+//     deleteItem.remove();
+// }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((item) => {
